40c9faf96e607ab7a0804ae41a3548e4800532fd
Cynthia Nnadiukwu <cynthiaamaran@gmail.com>
2023-03-24 22:13:43 +0100

    Chores: made some updates

--- src/Pages/about.js
+++ src/Pages/about.js
@@ -13,11 +13,14 @@ const About = () => {
           <Text fontSize="4xl" className="hello" textTransform="uppercase">
             Hello Welcome,
           </Text>
-          <Text py="2" fontSize="4xl" className="name">
-            My Name Is Nnadiukwu, Cynthia <br /> Amarachi
+          <Text py="2" fontSize="2xl" className="name">
+            My Name is Nnadiukwu, Cynthia Amarachi
           </Text>
-          <Text fontSize="2xl" className="hello">
-            I am a Software Engineer
+          <Text fontSize="1xl" className="hello">
+            A little about myself, I have hands-on experience contributing to
+            building applications <br /> as a front-end engineer intern using
+            HTML5, CSS3, SASS, ReactJs, NextJs, Git (Version Control) <br /> and
+            open to learning new technology stacks.
           </Text>
           <Button colorScheme="teal" variant="outline" mt="5">
             Download Cv
